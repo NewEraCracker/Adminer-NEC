dd20326b24705ec664d054cf0204c259e8295294
NewEraCracker <NewEraCracker@gmail.com>
2013-10-14 12:06:25 +0100

    Add other files

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+define('IN_SCRIPT', true);
+
+function adminer_object() {
+	include_once './inc/plugin.php';
+	include_once './inc/dump-bz2.php';
+	include_once './inc/dump-zip.php';
+	include_once './inc/dump-date.php';
+	include_once './inc/login-servers.php';
+
+	$plugins = array(
+		new AdminerDumpBz2,
+		new AdminerDumpZip,
+		new AdminerDumpDate,
+		new AdminerLoginServers(array('127.0.0.1'))
+	);
+
+	return new AdminerPlugin($plugins);
+}
+
+include './inc/adminer.php';
+?>
